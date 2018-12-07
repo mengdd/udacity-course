fb33a733fbb95e8c094c73f741df947392eff71f
Dandan MENG <mengdandanno1@163.com>
2018-12-07 15:10:02 +0800

    task06: user input, theme, my solution

--- course/06_input/task_06_input/lib/main.dart
+++ course/06_input/task_06_input/lib/main.dart
@@ -5,10 +5,11 @@
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
+import 'category_route.dart';
+
 // You can use a relative import, i.e. `import 'category_route.dart;'` or
 // a package import, as shown below.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
-import 'category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -25,8 +26,14 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      // TODO: Fill out the theme parameter
       home: CategoryRoute(),
+      theme: ThemeData(
+        primaryColor: Colors.grey[500],
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.grey[600],
+            ),
+      ),
     );
   }
 }
