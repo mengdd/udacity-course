aa5568b7aea66aebf715a687cd98adef06412a79
Dandan MENG <mengdandanno1@163.com>
2018-12-07 11:21:17 +0800

    task04: navigator to other screen, my solution

--- course/04_navigation/task_04_navigation/lib/converter_route.dart
+++ course/04_navigation/task_04_navigation/lib/converter_route.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
-
 import 'package:task_04_navigation/unit.dart';
 
 /// Converter screen where users can input amounts to convert.
@@ -16,19 +15,24 @@ import 'package:task_04_navigation/unit.dart';
 class ConverterRoute extends StatelessWidget {
   /// Units for this [Category].
   final List<Unit> units;
+  final String name;
+  final Color color;
 
   /// This [ConverterRoute] requires the name, color, and units to not be null.
-  // TODO: Pass in the [Category]'s name and color
   const ConverterRoute({
+    @required this.name,
+    @required this.color,
     @required this.units,
-  }) : assert(units != null);
+  })  : assert(name != null),
+        assert(color != null),
+        assert(units != null);
 
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
     final unitWidgets = units.map((Unit unit) {
-      // TODO: Set the color for this Container
       return Container(
+        color: color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
